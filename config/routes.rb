7be8f76441a9707f154a08ebfe3f4b3ede8f41e0
s-yasuoka <migayasu@gmail.com>
2022-04-05 00:52:16 +0000

    [Add]sign画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
   root :to => "public/homes#about"
 
   namespace :public do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: "a",
+  point: "10",
+  rank_status: "3",
+  is_paid: "false",
+  email: "a@a",
+  password: "000000"
+)
+
